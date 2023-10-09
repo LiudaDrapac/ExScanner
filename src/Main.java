51d21d434c833eea433d16fec668464927df2543
Ludmila <ludmila.drapac@cnas.gov.md>
2023-10-09 05:33:36 -0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            int numar = scanner.nextInt();
+            if (numar > 0) {
+            System.out.println("Numarul afisat este :" + numar);
+            }
+            else {
+            System.out.println("Iesire");
+            break;
+            }
+
+        }
     }
 }
+
+
+
